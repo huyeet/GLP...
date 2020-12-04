4a612111d4e96a1640051dcf06be8ecd095eb39d
huyeet <hnguyen21@pickeringcollege.on.ca>
2020-12-04 11:10:30 -0500

    Following tutorials. I need to work on my logical thinking

--- scene.py
+++ scene.py
@@ -6,5 +6,13 @@ from manim import *
 class SquareToCircle(Scene):
     def construct(self):
         circle = Circle()  # Create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(ShowCreation(circle))  # show the circle on screen
+        circle.set_fill(RED_A, opacity=0.5)  # set the color and transparency
+
+        square = Square()  # Create a square
+        square.flip()  # Flip Horizontally
+        square.rotate(-9 * TAU / 28)  # Rotate a certain amount TAU is essentially 2pi
+
+        self.play(ShowCreation(square))  # Animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # Fade out animation
+
